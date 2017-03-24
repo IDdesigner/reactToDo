c2688b064d62e64dd32cab5238d5e0c7fe347a5c
Rye Crowen <ryecrowendesign@mac.com>
2017-03-23 22:50:09 -0400

    Added filtering to todos in TodoAPI

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -32,7 +32,7 @@ var TodoApp = React.createClass({
   handleSearch(showCompleted, searchText) {
     this.setState({
       showCompleted: showCompleted,
-      searchText: searchText.toLowercase()
+      searchText: searchText.toLowerCase()
     })
   },
   handleToggle(id) {
@@ -48,11 +48,12 @@ var TodoApp = React.createClass({
     // alert(id);
   },
   render: function () {
-    var {todos} = this.state;
+    var {todos, showCompleted, searchText} = this.state;
+    var filteredTodos = TodoAPI.filterTodos(todos, showCompleted, searchText);
     return (
       <div>
         <TodoSearch onSearch={this.handleSearch}/>
-        <TodoList todos={todos} onToggle={this.handleToggle}/>
+        <TodoList todos={filteredTodos} onToggle={this.handleToggle}/>
         <AddTodo onAdd={this.handleAddTodo}/>
       </div>
     )
